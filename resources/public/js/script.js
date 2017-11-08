9148fcbe0b6160ce5f973b4a19eb230dc7e90fdd
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-11-08 10:43:55 -0500

    Fix sorting

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,16 +1,3 @@
 $(document).ready(function() {
-    $('#results-table').DataTable({
-        columnDefs: [
-            {
-                render: function(data) {
-                    if (data == "") {
-                        return "Price not available";
-                    } else {
-                        return data;
-                    }
-                },
-                targets: 0
-            }
-        ]
-    });
+    $('#results-table').DataTable();
 });
